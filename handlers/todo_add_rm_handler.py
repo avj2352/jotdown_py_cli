31e70a9345d5f1ff391839a178f1bcf1901fa29f
PRAMOD A JINGADE <avj2352@gmail.com>
2024-07-02 13:40:51 -0400

    Update todo_add_rm_handler.py

--- handlers/todo_add_rm_handler.py
+++ handlers/todo_add_rm_handler.py
@@ -3,16 +3,18 @@ Todo handler for commands
 - add
 - rm
 - clear
+- update
+- tag
 """
 from models.messages import TODO_ITEM_ADDED, LIST_EMPTY_NOTIFICATION, LIST_INDEX_OUT_OF_BOUNDS, TODO_ITEM_REMOVED, \
-    TODO_ITEMS_CLEARED
+    TODO_ITEMS_CLEARED, TODO_ITEM_DESC_UPDATED, TODO_ITEM_TAG_UPDATED
 from util.config import write_todo_list_to_file
 from typing import Optional, List
 from models.todo import Todo
 from datetime import datetime
 from rich import console
 from handlers.todo_list_handler import get_todo_list
-from util.helper import extract_word_with_at
+from util.helper import extract_word_with_at, extract_description
 
 console = console.Console()
 
@@ -56,5 +58,56 @@ def clear_todo_items():
         # persist to file
         write_todo_list_to_file([])
         return console.print(TODO_ITEMS_CLEARED)
+    except IOError:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
+
+
+def update_todo_description(pos: int, text: str):
+    # empty list check
+    todo_list: List[Todo] = get_todo_list()
+    if todo_list is None or len(todo_list) == 0:
+        return console.print(LIST_EMPTY_NOTIFICATION)
+    # guard check
+    pos_list = [obj['id'] for obj in todo_list]
+    if pos not in pos_list:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
+    try:
+        todo_record = next(obj for obj in todo_list if obj['id'] == pos)
+        curr_tag = todo_record['tag']
+        todo_record['desc'] = text
+        if curr_tag is not None or curr_tag != "":
+            todo_record['desc'] += " "
+            todo_record['desc'] += curr_tag
+        # persist to file
+        write_todo_list_to_file(todo_list)
+        return console.print(TODO_ITEM_DESC_UPDATED)
+    except StopIteration:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
+    except IOError:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
+
+
+def change_tag_annotation(pos: int, tag: str):
+    # empty list check
+    todo_list: List[Todo] = get_todo_list()
+    if todo_list is None or len(todo_list) == 0:
+        return console.print(LIST_EMPTY_NOTIFICATION)
+    # guard check
+    pos_list = [obj['id'] for obj in todo_list]
+    if pos not in pos_list:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
+    try:
+        todo_record = next(obj for obj in todo_list if obj['id'] == pos)
+        curr_desc = extract_description(todo_record['desc'])
+        if curr_desc is not None or curr_desc != "":
+            todo_record['desc'] = curr_desc
+            todo_record['desc'] += " "
+            todo_record['desc'] += tag
+            todo_record['tag'] = tag
+        # persist to file
+        write_todo_list_to_file(todo_list)
+        return console.print(TODO_ITEM_TAG_UPDATED)
+    except StopIteration:
+        return console.print(LIST_INDEX_OUT_OF_BOUNDS)
     except IOError:
         return console.print(LIST_INDEX_OUT_OF_BOUNDS)
